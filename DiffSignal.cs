a887c7344febe765ca7e07e4bf00f7b4073b6116
zccz14 <zccz14@outlook.com>
2017-12-13 01:20:35 +0800

    Init

---
+++ DiffSignal.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using QuantTC;
+using QuantTC.Indicators.Generic;
+using static System.Math;
+using static QuantTC.Functions;
+
+namespace QuantIX.Signals
+{
+    public class DiffSignal: Indicator<bool>
+    {
+        public DiffSignal(IIndicator<double> source, int period, double diff)
+        {
+            Source = source;
+            Period = period;
+            Diff = diff;
+            LastCount = 0;
+            Source.Update += Source_Update;
+        }
+
+        private void Source_Update()
+        {
+            Data.FillRange(Count, Source.Count, Calc);
+            FollowUp();
+        }
+
+        private bool Calc(int argI)
+        {
+            LastCount = Max(LastCount, argI - Period + 1);
+            var data = Source[argI];
+            var high = Range(LastCount, argI + 1).Max();
+            var low = Range(LastCount, argI + 1).Min();
+            var result = high - data > Diff || data - low > Diff;
+            if (result)
+            {
+                LastCount = Data.Count;
+            }
+            return result;
+        }
+
+        private IIndicator<double> Source { get; }
+        private int Period { get; }
+        private double Diff { get; }
+        private int LastCount { get; set; }
+    }
+}
